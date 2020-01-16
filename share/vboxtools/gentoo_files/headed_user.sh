dadd7933c2f4c52f23b9fa3192b24cad54cd721d
Justin Marsh (:flaviusb) <justin.marsh@flaviusb.net>
2020-01-16 16:00:24 +1300

    Create headed_user.sh
    Script to add a graphical login using LightDM

---
+++ share/vboxtools/gentoo_files/headed_user.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo emerge x11-apps/xdm x11-misc/lightdm
+sudo rc-update add xdm default
+
+echo "exec startxfce4" > ~/.xinitrc
+
+sudo sed -e "s/^DISPLAYMANAGER="xdm"$/DISPLAYMANAGER="lightdm"/" -i /etc/conf.d/xdm
